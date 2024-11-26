49a260eb07791b4193951c4179571875b0799106
MJS2103 <marshall.soanes@hamiltonjet.nz>
2024-11-26 15:12:05 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,11 +43,11 @@ if ingredients_list:
         
         filtered_values = pb_df.loc[pb_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
 
-        if not filtered_values.empty:
+        #if not filtered_values.empty:
               search_on = filtered_values.iloc[0]  # Access the first row safely
-              #st.write(f'The search value for {fruit_chosen} is {search_on}.')
-        else:
-              #st.write(f'No search value found for {fruit_chosen}.')
+              st.write(f'The search value for {fruit_chosen} is {search_on}.')
+        ##else:
+              st.write(f'No search value found for {fruit_chosen}.')
 
  
             
